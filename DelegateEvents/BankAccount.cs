ae72d1c1ea6caf59b4fde7c9f876bbf74ef56d50
evren <evren@EVRENAKTAS>
2024-02-25 04:11:41 +0300

    Delegate Events

---
+++ DelegateEvents/BankAccount.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateEvents
+{
+    public class BankAccount
+    {
+
+        public delegate void MinimumBalanceReachedHandler(BankAccount bankAccount, decimal amount);
+        public decimal NewBalance { get; set; }
+        public decimal Balance { get; private set; }
+        public decimal InternetShoppingLimit { get; set; }
+        public event MinimumBalanceReachedHandler MinimumBalanceReached;
+        public BankAccount(decimal initialBalance)
+        {
+            Balance = initialBalance;
+            InternetShoppingLimit = initialBalance;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (Balance >= amount)
+            {
+                NewBalance = Balance - amount;
+                Console.WriteLine($"Available balance: {NewBalance} TL");
+
+                if (NewBalance < 100)
+                    OnMinimumBalanceReached(Balance);
+
+
+            }
+            else
+                Console.WriteLine("Insufficient funds.");
+        }
+        public decimal Deposit(decimal balance)
+        {
+            return balance * 0.20m;
+        }
+        protected virtual void OnMinimumBalanceReached(decimal currentBalance)
+        {
+            MinimumBalanceReached.Invoke(this, currentBalance);
+        }
+    }
+}
---
+++ DelegateEvents/Program.cs
@@ -0,0 +1,25 @@
+﻿using DelegateEvents;
+
+internal class Program
+{
+    delegate void BankOperation(decimal amount);
+    static void Main(string[] args)
+    {
+        BankAccount account = new BankAccount(3000);
+        account.MinimumBalanceReached += IncreaseInternetShoppingLimit;
+        Console.WriteLine("Total shopping amount:");
+        var shoppingAmount = Convert.ToDecimal(Console.ReadLine());
+        BankOperation bankOperation = account.Withdraw;
+        bankOperation(shoppingAmount);
+        Console.ReadLine();
+    }
+    private static void IncreaseInternetShoppingLimit(BankAccount bankAccount, decimal currentBalance)
+    {
+        var account = bankAccount as BankAccount;
+        if (account != null)
+        {
+            account.InternetShoppingLimit += account.Deposit(account.Balance);
+            Console.WriteLine($"Your online shopping limit has been increased, new limit: {account.InternetShoppingLimit} TL");
+        }
+    }
+}
