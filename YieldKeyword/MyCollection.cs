47ca4c49ce95a903df8defeec525de36f04e56cc
evren <evren@EVRENAKTAS>
2024-02-26 21:32:51 +0300

    Yield Keyword

---
+++ YieldKeyword/MyCollection.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YieldKeyword
+{
+    public class MyCollection : IEnumerable, IEnumerator
+    {
+        private List<Book> books;
+        int position = -1;
+        public MyCollection(List<Book> books)
+        {
+           
+            this.books = books;
+         
+        }
+        public IEnumerator GetEnumerator()
+        {
+            return this;
+        }
+        public object Current
+        {
+            get
+            {
+                return books[position];
+            }
+        }
+        public bool MoveNext()
+        {
+            position +=2;
+            return (position < books.Count);
+        }
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        
+    }
+}
---
+++ YieldKeyword/Program.cs
@@ -0,0 +1,54 @@
+﻿
+using YieldKeyword;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+
+        List<Book> books = new List<Book> { new Book() { Author = "X", Title = "Y" }, new Book() { Author = "Z", Title = "C" }, new Book() { Author = "A", Title = "D" } };
+        MyCollection collection = new MyCollection(books);
+        foreach (var item in collection)
+        {
+            var book = item as Book;
+            Console.WriteLine(book.Title, book.Author);
+        }
+        foreach (var item in SettingsWithYield())
+        {
+            Console.WriteLine(item.key, item.value);
+        }
+        Console.ReadLine();
+    }
+    public static IEnumerable<int> GetEvenNumbers(int max)
+    {
+        for (int i = 0; i <= max; i++)
+        {
+            if (i % 2 == 0)
+                yield return i;
+        }
+    }
+    public static IEnumerable<(string key, string value)> SettingsWithYield()
+    {
+        yield return ("a", "1");
+        yield return ("b", "2");
+        yield return ("c", "3");
+    }
+    public static IEnumerable<(string key, string value)> Settings()
+    {
+        List<(string, string)> settings = new List<(string, string)>()
+            {
+                ("a","1"),("b","2"),("c","3")
+            };
+        return settings;
+    }
+    public static IEnumerable<int> GetNumbersUntil(int max, int stopAt)
+    {
+        for (int i = 0; i <= max; i++)
+        {
+            if (i == stopAt)
+                yield break; 
+            yield return i;
+        }
+    }
+}
