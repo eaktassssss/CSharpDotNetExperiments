63b587c4c1b5760e2099628f72149b5fb2d5eb28
evren <evren@EVRENAKTAS>
2024-02-28 01:50:29 +0300

    Secure String and Memory Security

---
+++ Reflection/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using MemorySecurity;
+using System.Runtime.InteropServices;
+using System.Security;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            BankCard bankCard = new BankCard();
+            bankCard.CardNo = "55555555555";
+            SecureString secureString = new SecureString();
+            bankCard.CardNo.ToList().ForEach(c => secureString.AppendChar(c));
+            secureString.MakeReadOnly();
+
+
+            IntPtr bstr = Marshal.SecureStringToBSTR(secureString);
+            string cardNo=Marshal.PtrToStringAnsi(bstr);
+
+        }
+        catch (Exception exception)
+        {
+            throw new Exception(exception.Message);
+        }
+        Console.ReadLine();
+    }
+}
